86e34682025cbab0e606f5821af1622ef6b79db0
nribravx <nribravx@gmail.com>
2022-01-13 22:28:41 +0700

    ud1

---
+++ main.js
@@ -0,0 +1,18 @@
+alert ('Chúc mừng 21 tủi')
+confirm ('co phai di Thi Thai khum?') //hien len thong bao xac nhan
+
+
+prompt ('z thi nhap passwork di') // hien le o thong bao xac nhan va khung input (alert + confirm) 
+var fullName = 'Nguyễn Thị Thãi'
+alert (fullName)
+
+console.log('Day la dong log') 
+
+// bat len thong bao alert, confirm, prompt 
+confirm 
+
+
+setTimeout(function () 
+{
+  alert('Thong bao')
+ }, 1000)  // set time de hien o thong bao
--- thai.js
+++
@@ -1,5 +0,0 @@
-alert ('Chúc mừng 21 tủi')
-confirm ('co phai di thi Thai khum?')
-prompt ('z thi nhap passwork di')
-var fullName = 'Nguyễn Thị Thãi'
-alert (fullName)
